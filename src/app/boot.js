a706b8cbda44b242b898e74e008d2f78a8377bec
Adrian Burcin <adrian.burcin@yahoo.com>
2016-01-30 20:42:30 +0200

    Service example

--- src/app/boot.js
+++ src/app/boot.js
@@ -1,3 +1,5 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {AppComponent} from './app.component';
+import { bootstrap } from 'angular2/platform/browser';
+
+import { AppComponent } from './app.component';
+
 bootstrap(AppComponent);
